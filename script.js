4d21d3058b14b76cc1355e824efd32ac78b5313a
louise <louise.ruffin@akteo.fr>
2026-08-10 15:57:05 +0200

    carte note libre avec animation bonus

--- script.js
+++ script.js
@@ -1340,7 +1340,7 @@ function getSelectedActionsFromState(state) {
 }
 
 function getOtherCategoryBonus(state) {
-  return state?.otherCategorySelected ? 0.3 : 0;
+  return state?.otherCategorySelected ? 0.5 : 0;
 }
 
 function computeRoomScore(state, selectedActions = getSelectedActionsFromState(state)) {
